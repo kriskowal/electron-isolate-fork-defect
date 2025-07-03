34658aba050bb703ec94878f04207b5d37e9f472
Kris Kowal <kris@cixar.com>
2025-07-03 10:12:20 -0700

    Disable ASAR

--- forge.config.js
+++ forge.config.js
@@ -3,7 +3,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
-    asar: true,
+    //asar: false,
   },
   rebuildConfig: {},
   makers: [
@@ -25,10 +25,10 @@ module.exports = {
     },
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
+    //{
+    //  name: '@electron-forge/plugin-auto-unpack-natives',
+    //  config: {},
+    //},
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
     new FusesPlugin({
@@ -37,8 +37,8 @@ module.exports = {
       [FuseV1Options.EnableCookieEncryption]: true,
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+      //[FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      //[FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
 };
